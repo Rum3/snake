14575d512d77ee6717b4b231205432c70d2aae5e
Rum3 <rum3n1992@gmail.com>
2022-11-01 13:10:18 +0200

    Updates

---
+++ precache-manifest.efe4c2c38946ccbc656501d3dbace44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268421526e16013b7fe8c586fe936be1",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "065c699c4d8a5d9fbe0a",
+    "url": "/snake/static/js/2.491d0b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.491d0b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7031eb26439fbd4813ef",
+    "url": "/snake/static/js/main.af57f266.chunk.js"
+  },
+  {
+    "revision": "757e336a23d9715a1cc8",
+    "url": "/snake/static/js/runtime-main.0646e259.js"
+  }
+]);
